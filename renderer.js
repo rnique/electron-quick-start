d6265aa6a2649cbbcd76453a5614b1c4ba7c1b71
Roman NL <rnique@steperu.com>
2018-09-21 00:31:25 -0500

    recarga de dependencias

--- renderer.js
+++ renderer.js
@@ -1,3 +1,21 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+var level = require('level-browserify')
+
+// 1) Create our database, supply location and options.
+//    This will create or open the underlying LevelDB store/Indexedb Database
+var db = level('./mydb')
+
+// 2) put a key & value
+db.put('name', 'Level', function (err) {
+  if (err) return console.log('Ooops!', err) // some kind of I/O error
+
+  // 3) fetch by key
+  db.get('name', function (err, value) {
+    if (err) return console.log('Ooops!', err) // likely the key was not found
+
+    // ta da!
+    console.log('name=' + value)
+  })
+})
